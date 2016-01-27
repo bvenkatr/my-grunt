33f4aaad2b024b1072eb6b848df36af9c10e2c98
Venkat <venkat.jaa@gamil.com>
2016-01-28 00:54:05 +0530

    read name from config

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,5 +1,11 @@
 module.exports = function(grunt){
+	grunt.initConfig({
+		person: {
+			firstName: "venkat"
+		}
+	});
+
 	grunt.registerTask("default", function(){
-		grunt.log.writeln("Hello World");
+		grunt.log.writeln("Hello " + grunt.config.get("person").firstName);
 	});
 };
---
+++ README.md
@@ -0,0 +1,7 @@
+module.exports = function(grunt){
+	grunt.registerTask("default", function(name){
+		grunt.log.writeln("Hello "+name);
+	});
+};
+grunt default:venkat
+/////////
