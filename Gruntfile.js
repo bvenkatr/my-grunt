62ad497ddec3c7859c92142b9c051dfdb7c5aadb
Venkat <venkat.jaa@gamil.com>
2016-01-28 01:03:26 +0530

    REad configs from filesystem

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,9 +1,5 @@
 module.exports = function(grunt){
-	grunt.initConfig({
-		person: {
-			firstName: "venkat"
-		}
-	});
+	grunt.initConfig(grunt.file.readJSON("config.json"));
 
 	grunt.registerTask("default", function(){
 		grunt.log.writeln("Hello " + grunt.config.get("person").firstName);
